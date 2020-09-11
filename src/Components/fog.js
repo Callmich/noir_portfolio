c929fa89dd0ebbc6f2bb9c723fc2911880ee4185
Michael Callahan <CallahanMichaelE@gmail.com>
2020-09-11 12:22:28 -0600

    working on setting up fog scene. Set base structure in component and adjusting css

--- src/Components/fog.js
+++ src/Components/fog.js
@@ -1,11 +1,22 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "../App.css"
 
 function Fog(){
     return (
-        <div>
-            <h1>You are in the fog!</h1>
-            <Link to={{pathname: '/office/'}}>To Office</Link>
+        <div class="foggy_city">
+            <div class="absolute-bg"></div>
+            <div class="fog-container">
+                <div class=" fog-img first_fog"></div>
+                <div class="fog-img second_fog"></div>
+            </div>
+
+            <div class="lamp_light"></div>
+
+            <div class="to_office">
+                <Link to={{pathname: '/office/'}}>To Office</Link>
+            </div>
+            
         </div>
     )
 }
